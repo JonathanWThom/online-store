687137c36fe8c017a13838a3cd3b42382e7e3602
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-22 16:24:08 -0800

    Add album object

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Album } from 'app/album.model';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
   styleUrls: ['./marketplace.component.css']
 })
-export class MarketplaceComponent {
 
+export class MarketplaceComponent {
+  masterAlbumList: Album[] = [
+   new Album('The Blue Album'),
+   new Album('The White Album')
+ ];
 }
